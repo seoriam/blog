8165340027015bb9eaf448f627e2bdff06dc7aff
seoriam <whs6441@gmail.com>
2019-05-15 09:29:20 +0900

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,11 +4,11 @@ module.exports = {
   publisher: 'Publisher named Seoriam', // Organization name used for SEO schema
   siteDescription:
     'A study on the web for Artificial Intelligence, Quantum Computing, Molecular Engineering, and Graphics',
-  siteUrl: 'https://xenodochial-kirch-4da5af.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
+  siteUrl: 'https://seoriam.netlify.com', // Site domain. Do not include a trailing slash! If you wish to use a path prefix you can read more about that here: https://www.gatsbyjs.org/docs/path-prefix/
   postsPerHomePage: 7, // Number of posts shown on the 1st page of of the index.js template (home page)
   postsPerPage: 6, // Number of posts shown on paginated pages
   author: 'Seoriam', // Author for RSS author segment and SEO schema
-  authorUrl: 'https://xenodochial-kirch-4da5af.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
+  authorUrl: 'https://seoriam.netlify.com/about/', // URL used for author and publisher schema, can be a social profile or other personal site
   userTwitter: '@twitter', // Change for Twitter Cards
   shortTitle: 'Seoriam Blog App', // Used for App manifest e.g. Mobile Home Screen
   shareImage: '/logos/share.svg', // Open Graph Default Share Image. 1200x1200 is recommended
